6cb5d288e1c0fd348612935cda45ee7fca4fdeb6
MalenBG <89998252+MalenBG@users.noreply.github.com>
2021-09-27 14:57:05 -0300

    Add files via upload

---
+++ bucles/app.js
@@ -0,0 +1,47 @@
+
+// 2) Solicitar al usuario que ingrese 5 números y mostrar la suma de ellos.
+
+// suma = 0;
+// for (n = 0; n < 5; n++) {
+//     s = prompt('Ingrese número' + n + ': ', '');
+//     suma += parseInt(s);
+//     alert(suma)
+// }
+
+// 4) Solicitar al usuario que ingrese números hasta que desee y luego mostrar la suma de ellos.
+
+// suma = 0;
+// var pepe = parseInt(window.prompt("¿Cuantos nùmeros desea ingresar?"))
+// for (n = 0; n < pepe; n++) {
+//     s = prompt('Ingrese número' + n + ': ', '');
+//     suma += parseInt(s);
+//     alert(suma)
+// }
+
+// 6) Realizar un programa que pida al usuario un número y muestre los números del 1 al número introducido.
+
+
+
+// 8) Preguntar al usuario cuántos números quiere ingresar. Luego solicitar que los ingrese uno por uno y mostrar cuál es el mayor.
+
+// var mayorA = mayorA;
+// var pepe = parseInt(window.prompt("¿Cuantos nùmeros desea ingresar?"))
+// for (n = 0; n < pepe; n++) {
+//     s = prompt('Ingrese número' + n + ': ', '');
+//     s = parseInt(s)
+//     if (mayorA === s || mayorA > s){
+//         alert('El mayor es: ' + mayorA)
+//     } else{
+//         mayorA = s
+//         alert('El mayor es: ' + mayorA)
+//     }
+// }
+
+// 10) Solicitar al usuario que ingrese un número y mostrar, en una sola cadena de string, la cantidad de asteriscos qué indique en el número. (Ejemplo: 5, “*****”.)
+
+// var asterico = ' ';
+// var pepe = parseInt(window.prompt("Ingrese la cantidad de astéricos recomendada por su médico."));
+// for (n = 0; n < pepe; n++) {
+//     asterico += '*'
+// }
+// alert('La cantidad de asteriscos es:' + asterico)
